8a67054cb993f2be05a9affb311f492c53797513
samekvitor(samuel lopes) <samek.vitor1@gmail.com>
2019-11-01 01:23:51 -0300

    dsa

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 <html>
 <head>
 	<title>Upload de Arquivos</title>
+  <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+            
 </head>
 <body>
 	<form action="salvarArquivo.php" method="POST" enctype="multipart/form-data">
@@ -11,5 +17,22 @@
 		<input type="file" name="arquivo"><br>
 		<input type="submit" name="enviar-formulario">	
 	</form>
+<div class="row">
+    <form class="col s12">
+      <div class="row">
+        <div class="input-field col s12">
+          <textarea id="textarea1" class="materialize-textarea"></textarea>
+          <label for="textarea1">Textarea</label>
+        </div>
+      </div>
+<div class="row">
+        <div class="input-field col s12">
+          <textarea id="textarea1" class="materialize-textarea"></textarea>
+          <label for="textarea1">Textarea</label>
+        </div>
+      </div>
+    </form>
+  </div>
+
 </body>
 </html>
