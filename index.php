95ae0a69d6f3ea3db5289e01ffaa8e5bdc37422d
Oseias Ferreira <oseiasf25@gmail.com>
2019-11-01 02:51:36 -0300

    alterações

--- index.php
+++ index.php
@@ -8,16 +8,9 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>    
 </head>
 <body>
-<<<<<<< HEAD
-  <form style="margin-left:30%;margin-right:30%;margin-top:10%; background-color:#F4F6F6; padding:3%; border-radius:2%;" action="salvarArquivo.php" method="POST" enctype="multipart/form-data">	
+  <form style="margin-left:30%;margin-right:30%;margin-top:10%; background-color:#F4F6F6; padding:3%; border-radius:2%;" action="salvarArquivo.php" method="POST" enctype="multipart/form-data">  
     <input name="nomeArquivo" id="motivo" placeholder="Insira o nome do arquivo..." type="text">
     <div class="file-field input-field">
-=======
-	<form style="margin-left:30%;margin-right:30%;margin-top:10%; background-color:#F4F6F6 ; padding:3%; border-radius:2%;" action="salvar.php" method="POST" enctype="multipart/form-data">
-		
-<input name = "nomeArquivo" id="motivo" placeholder="Insira o nome do arquivo..." type="text">
-<div class="file-field input-field">
->>>>>>> ab9f319fbb7263a80139d384eac5956df7ed378a
       <div class="btn">
         <span>Arquivo</span>
         <input type="file" name="arquivo">
