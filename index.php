41453c3c27ff4a2e5fd3d43e21c835daa889cab6
samekvitor(samuel lopes) <samek.vitor1@gmail.com>
2019-11-01 01:38:24 -0300

    teste

--- index.php
+++ index.php
@@ -11,26 +11,10 @@
 </head>
 <body>
 	<form action="salvarArquivo.php" method="POST" enctype="multipart/form-data">
-		<label>Nome do trabalho</label><input type="text" name="nomeArquivo">
-		Nome do arquivo sepadados por "_"<input type="text" name="nomeArquivo2">
-		<a href="listadeArquivos.php">Lista</a>
-		<input type="file" name="arquivo"><br>
-		<input type="submit" name="enviar-formulario">	
-	</form>
-<div class="row">
-    <form class="col s12">
-      <div class="row">
-        <div class="input-field col s12">
-          <textarea id="textarea1" class="materialize-textarea"></textarea>
-          <label for="textarea1">Textarea</label>
-        </div>
-      </div>
-<div class="row">
-        <div class="input-field col s12">
-          <textarea id="textarea1" class="materialize-textarea"></textarea>
-          <label for="textarea1">Textarea</label>
-        </div>
-      </div>
+		<input name="motivo" id="motivo" placeholder="Insira o motivo..." type="text">
+<input name = "nomeArquivo" id="motivo" placeholder="Insira o motivo..." type="text">
+<input name="motivo" id="motivo" placeholder="Insira o motivo..." type="text">
+<input name="motivo" id="motivo" placeholder="Insira o motivo..." type="file">
     </form>
   </div>
 
